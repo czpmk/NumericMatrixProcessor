9a505e73a43b836b48089d3665bbabbe762ff45c
Michal Czapiewski <czapiewskimk@gmail.com>
2020-08-16 18:43:51 +0200

    feat(Main.kt): adjusted for new project structures and features

--- src/processor/Main.kt
+++ src/processor/Main.kt
@@ -4,53 +4,122 @@ import java.lang.NumberFormatException
 import java.util.*
 
 val scanner = Scanner(System.`in`)
+const val menuMessage = "~  Available actions:\n" +
+        "1. Add matrices\n" +
+        "2. Multiply matrix to a constant\n" +
+        "3. Multiply matrices\n" +
+        "0. Exit\n"
 
-fun nextLineToInt(): MutableList<Int> {
-    val newLineSplit = scanner.nextLine().split(" ").filter { i -> i.isNotEmpty() }
-    val argumentList = mutableListOf<Int>()
-    for (argument in newLineSplit) {
+fun readSize(): Pair<Int, Int> {
+    while (true) {
+        print("~  Enter size of a matrix (rows, columns): ")
         try {
-            argumentList.add(argument.toInt())
+            val rows = scanner.nextInt()
+            val columns = scanner.nextInt()
+            if (rows < 1 || columns < 1) {
+                println("Invalid size, required rows <= 1 columns <= 1")
+                continue
+            }
+            return Pair(rows, columns)
         } catch (e: NumberFormatException) {
-            continue
+            println("Invalid value, integer required")
         }
-    }
-    return argumentList
-}
-
-fun readSize(): Triple<Boolean, Int, Int> {
-    val numbers = nextLineToInt()
-    return if (numbers.size == 2) {
-        val rows = numbers[0]
-        val columns = numbers[1]
-        Triple(true, rows, columns)
-    } else {
-        Triple(false, 0, 0)
     }
 }
 
 fun readMatrix(numberOfRows: Int, numberOfColumns: Int): Matrix {
-    val newMatrix = Matrix()
-    for (row in 0 until numberOfRows) {
-        val newRow = nextLineToInt()
-        if (newRow.size == numberOfColumns) {
+    println("~  Enter first matrix ($numberOfRows x $numberOfColumns): ")
+    while (true) {
+        val newMatrix = Matrix()
+        while (newMatrix.rows < numberOfRows) {
+            val newRow = mutableListOf<Element>()
+            while (newRow.size < numberOfColumns) {
+                newRow.add(Element(scanner.next()))
+            }
             newMatrix.value.add(newRow)
+        }
+        if (newMatrix.isValid) {
+            return newMatrix
         } else {
-            return Matrix()
+            println("Matrix contains invalid elements: \n")
+            for (row in newMatrix.value) {
+                print(row.filter{i -> !i.isValid}.joinToString(" ") + " ")
+            }
+            println("\n~ Please, enter the matrix once again: ")
         }
     }
-    return newMatrix
+}
+
+fun readConstant(): Element {
+    print("~  Enter the constant: ")
+    while (true) {
+        val constant = Element(scanner.next())
+        if (constant.isValid) {
+            return constant
+        } else {
+            println("The number $constant is not valid")
+            println("\n~ Please, enter the constant once again: ")
+        }
+    }
+
+}
+
+fun additionPipeline() {
+    val (firstRows, firstColumns) = readSize()
+    val firstMatrix = readMatrix(firstRows, firstColumns)
+    val (secondRows, secondColumns) = readSize()
+    val secondMatrix = readMatrix(secondRows, secondColumns)
+    when {
+        !firstMatrix.sameSizeAs(secondMatrix) -> {
+            println("Error, matrices sizes do not match!")
+        }
+        else -> {
+            println("The addition result:")
+            firstMatrix.sum(secondMatrix).print()
+        }
+    }
+}
+
+fun constantMultiplicationPipeline() {
+    val (numberOfRows, numberOfColumns) = readSize()
+    val firstMatrix = readMatrix(numberOfRows, numberOfColumns)
+    val constant = readConstant()
+    println("\nThe multiplication result:")
+    firstMatrix.constantMultiplication(constant).print()
+}
+
+fun matrixMultiplicationPipeline() {
+    val (firstRows, firstColumns) = readSize()
+    val firstMatrix = readMatrix(firstRows, firstColumns)
+    val (secondRows, secondColumns) = readSize()
+    val secondMatrix = readMatrix(secondRows, secondColumns)
+    when {
+        !firstMatrix.ableToMultiplyBy(secondMatrix) -> {
+            println("Error, matrices sizes do not allow for multiplication!")
+        }
+        else -> {
+            println("The multiplication result:")
+            firstMatrix.matrixMultiplication(secondMatrix).print()
+        }
+    }
 }
 
 fun nextAction() {
-    val matrixList = mutableListOf<Matrix>()
-    val (good, rows, columns) = readSize()
-    if (good) matrixList.add(readMatrix(rows, columns))
-    val factor = scanner.nextInt()
-    when {
-        matrixList.size != 1 -> println("ERROR")
-//        !Processor.compareBySize(matrixList[0], matrixList[1]) -> println("ERROR")
-        else -> Processor.scalarMultiplication(matrixList[0], factor).print()
+    println("~  Welcome to Numeric Matrix Processor, have fun!")
+    actionLoop@ while (true) {
+        print(menuMessage)
+        print("~  Your choice: ")
+        when (val actionIdentifier = scanner.next()) {
+            "0" -> {
+                println("~  Bye!")
+                break@actionLoop
+            }
+            "1" -> additionPipeline()
+            "2" -> constantMultiplicationPipeline()
+            "3" -> matrixMultiplicationPipeline()
+            else -> println("Invalid action: There is no action = $actionIdentifier")
+        }
+        println()
     }
 }
 
